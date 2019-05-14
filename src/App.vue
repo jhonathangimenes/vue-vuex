39c5365fabb29701b4f342bc5b55d27f8864c2a7
unknown <gimenesjhonathan@gmail.com>
2019-05-14 00:16:43 -0300

    Instalação do vue e vuex

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
+    </div>
+    <div class="container">
+      <Contador/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Contador from '@/components/Contador.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Contador
+  }
+}
+</script>
---
+++ src/components/Contador.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h3 class="font-weight-light">Contador {{ $store.state.contador }}</h3>
+        <button class="btn btn-danger" @click="$store.state.contador--">-</button>
+        <button class="btn btn-success" @click="$store.state.contador++">+</button>
+    </div>
+</template>
